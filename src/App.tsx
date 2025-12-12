c8da735b4534c859b3a592826dd5979a4169ac2b
Iliyas128 <weking128@gmail.com>
2025-12-12 22:47:33 +0500

    disablescroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        expand: () => void;
+        enableClosingConfirmation: () => void;
+        disableVerticalSwipes: () => void;
+      };
+    };
+  }
+}
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Leaders from "./pages/Leaders";
@@ -17,8 +29,12 @@ const queryClient = new QueryClient();
 
 const App = () => {
   useEffect(() => {
-    const tg = (window as any)?.Telegram?.WebApp;
-    tg?.disableVerticalSwipes?.();
+    // Расширяем WebApp на весь экран и выключаем вертикальные свайпы
+    if (window.Telegram?.WebApp) {
+      window.Telegram.WebApp.expand();
+      window.Telegram.WebApp.enableClosingConfirmation();
+      window.Telegram.WebApp.disableVerticalSwipes();
+    }
   }, []);
 
   return (
